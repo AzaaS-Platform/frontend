e08c9aa7a4b0907733edb1f086c40d06f4a47cbf
Ju5tMyWo4d <sztenk07+github2@gmail.com>
2020-04-11 23:37:46 +0200

    AP-28: Wygląd dodania roli

--- src/components/pages/AddRole.vue
+++ src/components/pages/AddRole.vue
@@ -1,5 +1,20 @@
 <template>
-    <div></div>
+    <div class="mdl-layout">
+        <form action="#" class="mdl-grid">
+            <div class="mdl-cell mdl-cell--middle">
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                    <input class="mdl-textfield__input" type="text" id="name">
+                    <label class="mdl-textfield__label" for="name">Nazwa roli...</label>
+                </div>
+            </div>
+            <div class="mdl-cell">
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                    <input class="mdl-textfield__input" type="text" id="sample3">
+                    <label class="mdl-textfield__label" for="sample3">Text...</label>
+                </div>
+            </div>
+        </form>
+    </div>
 </template>
 
 <script>
