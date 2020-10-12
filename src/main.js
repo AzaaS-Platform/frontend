cc642e34fcab5400b471ec5b4086c34edb9eb9da
Ju5tMyWo4d <ju5tmywo4d@gmail.com>
2020-10-12 02:33:37 +0200

    AP-46: Usunięcie niepotrzebnego dependency

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router';
-import {Plugin} from 'vue-fragment'
 import App from './App.vue'
 import routes from './main/routes.js';
 
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
-Vue.use(Plugin)
 
 
 const router = new VueRouter({
